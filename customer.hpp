2a82db497e0ce61600c4d1cc558030dbb3fe9677
spl15 <46474037+spl15@users.noreply.github.com>
2020-02-04 15:45:42 -0600

    Add files via upload

---
+++ customer.hpp
@@ -0,0 +1,24 @@
+/*
+ *      File  : customer.hpp
+ *      Author: Stephen Lamalie
+ *      Course: COP4534
+ *      Proj  : Project 01
+ */
+ #ifndef CUSTOMER_HPP
+#define CUSTOMER_HPP
+#include <stdlib>
+#include <stdio>
+
+class customer
+{
+    float arrivalTime;
+    float startOfService;
+    float departTime;
+    Customer* nextCustomer;
+};
+
+
+
+
+
+#endif
